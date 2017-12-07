aa0cc6e592baf786f4fa68855ece356fa6b0f693
Drew Miley <drew.miley@opencastsoftware.com>
2017-12-07 16:15:37 +0000

    Adds timings

--- app.js
+++ app.js
@@ -26,15 +26,25 @@ var lunrSearchID = lunr(function() {
 	}, this)
 });
 
+var t0 = performance.now();
+
 const idSearchResults = lunrSearchID.search('*000AB234*');
 
+var t1 = performance.now();
+
 console.log(idSearchResults);
+console.log(t1 - t0);
+
+t0 = performance.now();
 
 const jqIDsearchResults = $.grep(documents, d => {
 	return d.ID.search(new RegExp(/(.?)000AB234(.?)/)) !== -1;
 });
 
+t1 = performance.now();
+
 console.log(jqIDsearchResults);
+console.log(t1 - t0);
 
 var lunrSearchName = lunr(function() {
 	this.ref('id')
@@ -45,15 +55,25 @@ var lunrSearchName = lunr(function() {
 	}, this)
 });
 
+t0 = performance.now();
+
 const nameSearchResults = lunrSearchName.search('Part Apple');
 
+t1 = performance.now();
+
 console.log(nameSearchResults);
+console.log(t1 - t0);
+
+t0 = performance.now();
 
 const jqnameSearchResults = $.grep(documents, d => {
 	return d.name.search(new RegExp(/((.?)Part(.?)Apple(.?))|((.?)Apple(.?)Part(.?))/)) !== -1;
 });
 
+t1 = performance.now();
+
 console.log(jqnameSearchResults);
+console.log(t1 - t0);
 
 var lunrSearchKeywords = lunr(function() {
 	this.ref('id')
@@ -64,12 +84,22 @@ var lunrSearchKeywords = lunr(function() {
 	}, this)
 });
 
+t0 = performance.now();
+
 const keywordsSearchResults = lunrSearchKeywords.search('Houmous');
 
+t1 = performance.now();
+
 console.log(keywordsSearchResults);
+console.log(t1 - t0);
+
+t0 = performance.now();
 
 const jqKeywordsSearchResults = $.grep(documents, d => {
 	return d.keywords.includes('Houmous');
 });
 
+t1 = performance.now();
+
 console.log(jqKeywordsSearchResults);
+console.log(t1 - t0);
