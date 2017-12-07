78ad6fd88af9bbfd475c769e612bd25303ddc485
Drew Miley <drew.miley@opencastsoftware.com>
2017-12-07 16:26:00 +0000

    Slight refactor of calls

--- app.js
+++ app.js
@@ -26,26 +26,6 @@ var lunrSearchID = lunr(function() {
 	}, this)
 });
 
-var t0 = performance.now();
-
-const idSearchResults = lunrSearchID.search('*000AB234*');
-
-var t1 = performance.now();
-
-console.log(idSearchResults);
-console.log(t1 - t0);
-
-t0 = performance.now();
-
-const jqIDsearchResults = $.grep(documents, d => {
-	return d.ID.search(new RegExp(/(.?)000AB234(.?)/)) !== -1;
-});
-
-t1 = performance.now();
-
-console.log(jqIDsearchResults);
-console.log(t1 - t0);
-
 var lunrSearchName = lunr(function() {
 	this.ref('id')
 	this.field('name')
@@ -54,26 +34,6 @@ var lunrSearchName = lunr(function() {
 		this.add(doc)
 	}, this)
 });
-
-t0 = performance.now();
-
-const nameSearchResults = lunrSearchName.search('Part Apple');
-
-t1 = performance.now();
-
-console.log(nameSearchResults);
-console.log(t1 - t0);
-
-t0 = performance.now();
-
-const jqnameSearchResults = $.grep(documents, d => {
-	return d.name.search(new RegExp(/((.?)Part(.?)Apple(.?))|((.?)Apple(.?)Part(.?))/)) !== -1;
-});
-
-t1 = performance.now();
-
-console.log(jqnameSearchResults);
-console.log(t1 - t0);
 
 var lunrSearchKeywords = lunr(function() {
 	this.ref('id')
@@ -84,22 +44,69 @@ var lunrSearchKeywords = lunr(function() {
 	}, this)
 });
 
-t0 = performance.now();
+const logLunrSearchResultsId = () => {
+	const t0 = performance.now();
+	const searchResults = lunrSearchID.search('*000AB234*');
+	const t1 = performance.now();
+	console.log('Lunr Search Results: id');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-const keywordsSearchResults = lunrSearchKeywords.search('Houmous');
+const logJquerySearchResultsId = () => {
+	const t0 = performance.now();
+	const searchResults = $.grep(documents, d => {
+		return d.ID.search(new RegExp(/(.?)000AB234(.?)/)) !== -1;
+	});
+	const t1 = performance.now();
+	console.log('Jquery Search Results: id');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-t1 = performance.now();
+const logLunrSearchResultsName = () => {
+	const t0 = performance.now();
+	const searchResults = lunrSearchName.search('Part Apple');
+	const t1 = performance.now();
+	console.log('Lunr Search Results: name');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-console.log(keywordsSearchResults);
-console.log(t1 - t0);
+const logJquerySearchResultsName = () => {
+	const t0 = performance.now();
+	const searchResults = $.grep(documents, d => {
+		return d.name.search(new RegExp(/((.?)Part(.?)Apple(.?))|((.?)Apple(.?)Part(.?))/)) !== -1;
+	});
+	const t1 = performance.now();
+	console.log('Jquery Search Results: name');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-t0 = performance.now();
+const logLunrSearchResultsKeywords = () => {
+	const t0 = performance.now();
+	const searchResults = lunrSearchKeywords.search('Houmous');
+	const t1 = performance.now();
+	console.log('Lunr Search Results: keywords');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-const jqKeywordsSearchResults = $.grep(documents, d => {
-	return d.keywords.includes('Houmous');
-});
+const logJquerySearchResultsKeywords = () => {
+	const t0 = performance.now();
+	const searchResults = $.grep(documents, d => {
+		return d.keywords.includes('Houmous');
+	});
+	const t1 = performance.now();
+	console.log('Jquery Search Results: keywords');
+	console.log(searchResults);
+	console.log(t1 - t0);
+}
 
-t1 = performance.now();
-
-console.log(jqKeywordsSearchResults);
-console.log(t1 - t0);
+logLunrSearchResultsId();
+logJquerySearchResultsId();
+logLunrSearchResultsName();
+logJquerySearchResultsName();
+logLunrSearchResultsKeywords();
+logJquerySearchResultsKeywords();
